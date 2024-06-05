06ff36d5b58e5ad5019f8827d6604359a3028642
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-06-05 10:40:21 +0600

    1.6-completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,31 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <section id="feedback">
+        <h1>give feedback</h1>
+        <button type="button" onClick={() => setGood(good + 1)}>
+          good
+        </button>
+        <button type="button" onClick={() => setNeutral(neutral + 1)}>
+          neutral
+        </button>
+        <button type="button" onClick={() => setBad(bad + 1)}>
+          bad
+        </button>
+      </section>
+      <section id="statistics">
+        <h1>statistics</h1>
+        <p>
+          good {good}
+          <br />
+          neutral {neutral}
+          <br />
+          bad {bad}
+        </p>
+      </section>
+    </div>
+  );
 };
 export default App;
