c3ee12534c46a4c7ca86a6640e9cc14fb28a1726
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-06-06 18:31:54 +0600

    2.11-completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Filter = ({ filterTerm, setFilterTerm }) => {
   return (
@@ -61,12 +62,13 @@ const Persons = ({ filteredpersons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://172.25.20.171:3001/persons")
+      .then((response) => setPersons(response.data));
+  }, []);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterTerm, setFilterTerm] = useState("");
