44e4d1a0935fa69e8c3a33bf080ac11a57824544
Nazmul Hasan Shajib <shojib116@gmail.com>
2024-06-09 14:23:14 +0600

    2.19-completed

--- part2/dataforcountries/src/App.jsx
+++ part2/dataforcountries/src/App.jsx
@@ -1,6 +1,42 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+const CountryView = ({ country }) => {
+  const languages = [];
+  for (const key of Object.keys(country.languages)) {
+    languages.push(country.languages[key]);
+  }
+
+  return (
+    <div>
+      <h2>{country.name.common}</h2>
+      <div>capital {country.capital}</div>
+      <div>area {country.area}</div>
+      <h4>languages:</h4>
+      <ul>
+        {languages.map((language) => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+      <img src={country.flags.png} alt={country.flags.alt} />
+    </div>
+  );
+};
+
+const MultiCountryView = ({ country }) => {
+  const [show, setShow] = useState();
+
+  return (
+    <div>
+      {country.name.common}{" "}
+      <button type="button" onClick={() => setShow(!show)}>
+        {show ? "hide" : "show"}
+      </button>
+      {show && <CountryView country={country} />}
+    </div>
+  );
+};
+
 const Search = ({ searchTerm, handleTermChange, disabled }) => {
   return (
     <div>
@@ -21,7 +57,9 @@ const Result = ({ searchTerm, countries }) => {
       country.name.common.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
   );
 
-  if (!searchTerm || filteredCountries.length === 0) return null;
+  if (!searchTerm) return null;
+
+  if (filteredCountries.length === 0) return <p>No countries found</p>;
 
   if (filteredCountries.length > 10)
     return <div>Too many matches, specify another filter</div>;
@@ -30,31 +68,16 @@ const Result = ({ searchTerm, countries }) => {
     return (
       <div>
         {filteredCountries.map((country) => {
-          return <div key={country.name.common}>{country.name.common}</div>;
+          return (
+            <MultiCountryView country={country} key={country.name.common} />
+          );
         })}
       </div>
     );
 
   const country = filteredCountries[0];
 
-  const languages = [];
-  for (const key of Object.keys(country.languages)) {
-    languages.push(country.languages[key]);
-  }
-  return (
-    <div>
-      <h2>{country.name.common}</h2>
-      <div>capital {country.capital}</div>
-      <div>area {country.area}</div>
-      <h4>languages:</h4>
-      <ul>
-        {languages.map((language) => (
-          <li key={language}>{language}</li>
-        ))}
-      </ul>
-      <img src={country.flags.png} alt={country.flags.alt} />
-    </div>
-  );
+  return <CountryView country={country} />;
 };
 
 const App = () => {
